5f0e64bf13bb33ecb6ef3b79710f7647ab4147df
Sebastian Lopez <sebas.tmk@gmail.com>
2022-12-06 08:23:34 -0300

    jobProfile 0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ExperienceComponent } from './component/experience/experience.component';
+import { AboutComponent } from './component/about/about.component';
+import { EducationComponent } from './component/education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ExperienceComponent,
+    AboutComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
